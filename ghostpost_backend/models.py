f8a856e3ddb2e4460c9a764f191c037bfb405719
Doug Enas <douglassenas@gmail.com>
2019-06-26 13:39:57 -0400

    added datetimefield to model

--- ghostpost_backend/models.py
+++ ghostpost_backend/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from datetime import datetime
 
 class BoastsAndRoasts(models.Model):
-    boasts = models.CharField(max_length=1000)
-    roasts = models.CharField(max_length=1000)
+    boasts = models.CharField(blank=True, max_length=1000, default="boast?")
+    roasts = models.CharField(blank=True, max_length=1000, default="roast?")
     vote = models.BooleanField(default=False)
+    dateCreated = models.DateTimeField(default=datetime.now(), blank=True)
--- ghostpost_backend/serializers.py
+++ ghostpost_backend/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from ghostpost_backend.models import BoastsAndRoasts
 
 class BaRSerializer(serializers.ModelSerializer):
+    boasts = serializers.JSONField()
+    roasts = serializers.JSONField()
     class Meta:
         model = BoastsAndRoasts
-        fields = ('boasts', 'roasts', 'vote')
+        fields = ('boasts', 'roasts', 'vote', 'dateCreated')
